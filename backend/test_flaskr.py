1d7daf4c535b287ef3dc3b2a88b8db9dbdca607f
Sangyoon Park <datumvitae@gmail.com>
2021-01-07 07:29:53 -0500

    Add test: test_retrieve_categories()

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -8,31 +8,34 @@ from models import setup_db, Question, Category
 
 
 class TriviaTestCase(unittest.TestCase):
-    """This class represents the trivia test case"""
-
     def setUp(self):
-        """Define test variables and initialize app."""
+        """Define test variables and initialize app"""
+
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
         self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
-        # binds the app to the current context
+        # Bind the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    def test_retrieve_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsInstance(data['categories'], dict)
+        self.assertEqual(len(data['categories']), 6)
 
 
 # Make the tests conveniently executable
