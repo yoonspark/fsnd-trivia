7ed18aba86e9f283951bc2ca9bb50c7e6f2a46dd
Sangyoon Park <datumvitae@gmail.com>
2021-01-07 08:10:02 -0500

    Add tests for retrieving questions

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -37,6 +37,27 @@ class TriviaTestCase(unittest.TestCase):
         self.assertIsInstance(data['categories'], dict)
         self.assertEqual(len(data['categories']), 6)
 
+    def test_retrieve_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['current_category'])
+        self.assertIsInstance(data['categories'], dict)
+        self.assertIsInstance(data['questions'], list)
+        self.assertIsInstance(data['total_questions'], int)
+        self.assertEqual(len(data['categories']), 6)
+        self.assertEqual(len(data['questions']), 10)
+
+    def test_paginate_beyond_valid_page(self):
+        res = self.client().get('/questions?page=100')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
